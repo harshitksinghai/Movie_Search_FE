610fee111c9121f980c9ed7c86fd0f04d479d973
Harshit Kumar Singhai <hksinghai@ymsl.in>
2025-02-23 16:06:22 +0530

    stable

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
-import HomeScreen from './screens/HomeScreen.tsx'
+import HomeScreen from './screens/HomeScreen'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
